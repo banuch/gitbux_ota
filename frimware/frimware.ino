638498464f9a5909f0ba92c1202b19f2fb60398e
banuch <banu.chitte@gmail.com>
2026-06-19 17:17:27 +0530

    v1.0.1

--- frimware/frimware.ino
+++ frimware/frimware.ino
@@ -7,7 +7,7 @@ const char* ssid = "Sahasra"; // put your wifi name
 const char* password = "wintek@143"; // put your wifi password
 
 const char* firmwareUrl = "https://github.com/banuch/gitbux_ota/blob/master/frimware/build/esp32.esp32.esp32/frimware.ino.bin";
-const char* versionUrl = "https://raw.githubusercontent.com/ittipu/esp32_firmware/refs/heads/main/version.txt";
+const char* versionUrl = "https://raw.githubusercontent.com/banuch/gitbux_ota/refs/heads/master/frimware/version.txt";
 
 // Current firmware version
 const char* currentFirmwareVersion = "1.0.1";
